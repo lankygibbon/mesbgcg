a8de7a7d20a1c936bbf8046315c71e4542dbae94
Matt Seymour <matthewjamesseymour@gmail.com>
2022-11-21 22:25:45 +0000

    Stat line is now a table.

--- src/routes/CardTemplate.svelte
+++ src/routes/CardTemplate.svelte
@@ -1,20 +1,43 @@
 <script lang="ts">
-    import type { Card } from "../types/card.type";
-    export let card: Card;
+  import type { Card } from "../types/card.type";
+  export let card: Card;
 </script>
 
 <div class="aspect-mtg bg-white p-1">
-    <div class="bg-slate-500 p-3 rounded">
-        <div class="text-lg">{card.name}</div>
-        <div class="text-md">{card.keywords}</div>
-    </div>
-    <span>{card.faction}</span>
-    <div />
-    Mv<span>{card.mv}</span>
-    F<span>{card.f}/{card.fs}+</span>
-    S<span>{card.s}</span>
-    D<span>{card.d}</span>
-    A<span>{card.a}</span>
-    W<span>{card.w}</span>
-    C<span>{card.c}</span>
+  <div class="bg-slate-500 p-3 rounded">
+    <div class="text-lg">{card.name}</div>
+    <div class="text-md">{card.keywords}</div>
+  </div>
+  <div class="py-1">
+    <table class="table-auto w-full text-center">
+      <thead class="bg-slate-500">
+        <tr>
+          <th>Mv</th>
+          <th>F</th>
+          <th>S</th>
+          <th>D</th>
+          <th>A</th>
+          <th>W</th>
+          <th>C</th>
+          <th>Mi</th>
+          <th>Wi</th>
+          <th>Ft</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{card.mv}"</td>
+          <td>{card.f}/{card.fs}+</td>
+          <td>{card.s}</td>
+          <td>{card.d}</td>
+          <td>{card.a}</td>
+          <td>{card.w}</td>
+          <td>{card.c}</td>
+          <td>{card.might}</td>
+          <td>{card.will}</td>
+          <td>{card.fate}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
